103aae1368cb5c10873aa05b2cdfb01664104f7e
sergey <a-merz@yandex.ru>
2016-11-09 00:44:47 +0700

    filling collections (finished, notFinished, today)

--- diary/App.xaml.cs
+++ diary/App.xaml.cs
@@ -13,5 +13,43 @@ namespace diary
     /// </summary>
     public partial class App : Application
     {
+        private TasksDataContext __tasksDataContext;
+        public TasksDataContext tasksDataContext
+        {
+            get { return __tasksDataContext; }
+        }
+        private List<Tasks> __notFinished;
+        public List<Tasks> notFinished
+        {
+            get{ return __notFinished; }
+            set { __notFinished = value; }
+        }
+        private List<Tasks> __today;
+        public List<Tasks> today
+        {
+            get { return __today; }
+            set { __today = value; }
+        }
+        private List<Tasks> __finished;
+        public List<Tasks> finished
+        {
+            get { return __finished; }
+            set { __finished = value; }
+        }
+        private void InitTables()
+        {
+            var queryNotFinished = from c in tasksDataContext.tableTasks
+                        where c.Status1.name != "finished"
+                        select c;
+            notFinished = queryNotFinished.ToList<Tasks>();
+            var queryToday = from c in tasksDataContext.tableTasks
+                             where c.Status1.name == "inTodayList"
+                             select c;
+            today = queryToday.ToList<Tasks>();
+            var queryFinished = from c in tasksDataContext.tableTasks
+                                where c.Status1.name == "finished"
+                                select c;
+            finished = queryFinished.ToList<Tasks>();
+        }
     }
 }
