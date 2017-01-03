001e0463f315b5efa144810b9fb2804039d11400
sergey <a-merz@yandex.ru>
2017-01-04 00:49:14 +0700

    observableCollection + tab property

--- diary/App.xaml.cs
+++ diary/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Collections.ObjectModel;
 
 namespace diary
 {
@@ -18,23 +19,29 @@ namespace diary
             LoadTables();
         }
         private static TasksDataContext tasksDataContext = new TasksDataContext();
-        public static List<Tasks> notFinished { get; set; }
-        public static List<Tasks> today { get; set; }
-        public static List<Tasks> finished { get; set; }
+        //public static List<Tasks> inList { get; set; }
+        public static ObservableCollection<Tasks> inList { get; set; }
+        //public static List<Tasks> today { get; set; }
+        public static ObservableCollection<Tasks> today { get; set; }
+        //public static List<Tasks> finished { get; set; }
+        public static ObservableCollection<Tasks> finished { get; set; }
         public void LoadTables()
         {
-            IEnumerable<Tasks> queryNotFinished = from tb in tasksDataContext.tableTasks
-                        where tb.Status1.name.ToString() != "finished"
+            IEnumerable<Tasks> queryinList = from tb in tasksDataContext.Tasks
+                        where tb.list.ToString() == "inList"
                         select tb;
-            notFinished = queryNotFinished.ToList();
-            IEnumerable<Tasks> queryToday = from tb in tasksDataContext.tableTasks
-                             where tb.Status1.name.ToString() == "inTodayList"
-                             select tb;
-            today = queryToday.ToList();
-            IEnumerable<Tasks> queryFinished = from tb in tasksDataContext.tableTasks
-                                where tb.Status1.name.ToString() == "finished"
-                                select tb;
-            finished = queryFinished.ToList();
+            inList = new ObservableCollection<Tasks>(queryinList.ToList<Tasks>());
+            //inList = queryinList.ToList<Tasks>();
+            IEnumerable<Tasks> queryToday = from tb in tasksDataContext.Tasks
+                        where tb.list.ToString() == "inTodayList"
+                        select tb;
+            today = new ObservableCollection<Tasks>(queryToday.ToList<Tasks>());
+            //today = queryToday.ToList();
+            IEnumerable<Tasks> queryFinished = from tb in tasksDataContext.Tasks
+                        where tb.list.ToString() == "finished"
+                        select tb;
+            finished = new ObservableCollection<Tasks>(queryFinished.ToList<Tasks>());
+            //finished = queryFinished.ToList();
 
         }   
     }
